eda8d9a17bd6538c77415808d9a4ec958fb9af8a
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2022-08-17 23:09:11 -0500

    Commenting out geolocation functions for testing.

--- flux_common.sh
+++ flux_common.sh
@@ -166,173 +166,173 @@ function cdn_speedtest() {
     echo -e ""
 }
 
-function bootstrap_server(){
-    rand_by_domain=("5" "6" "7" "8" "9" "10" "11" "12")
-    richable=()
-    richable_eu=()
-    richable_us=()
-    richable_as=()
+# function bootstrap_server(){
+#     rand_by_domain=("5" "6" "7" "8" "9" "10" "11" "12")
+#     richable=()
+#     richable_eu=()
+#     richable_us=()
+#     richable_as=()
 
-    i=0
-    len=${#rand_by_domain[@]}
-    echo -e "${ARROW} ${CYAN}Checking servers availability... ${NC}"
-    while [ $i -lt $len ];
-    do
-        bootstrap_check=$(curl -sSL -m 10 http://cdn-${rand_by_domain[$i]}.runonflux.io/apps/fluxshare/getfile/flux_explorer_bootstrap.json 2>/dev/null | jq -r '.block_height' 2>/dev/null)
-        if [[ "$bootstrap_check" != "" ]]; then
+#     i=0
+#     len=${#rand_by_domain[@]}
+#     echo -e "${ARROW} ${CYAN}Checking servers availability... ${NC}"
+#     while [ $i -lt $len ];
+#     do
+#         bootstrap_check=$(curl -sSL -m 10 http://cdn-${rand_by_domain[$i]}.runonflux.io/apps/fluxshare/getfile/flux_explorer_bootstrap.json 2>/dev/null | jq -r '.block_height' 2>/dev/null)
+#         if [[ "$bootstrap_check" != "" ]]; then
 
-            if [[ "${rand_by_domain[$i]}" -ge "8" && "${rand_by_domain[$i]}" -le "11" ]]; then
-                richable_eu+=( ${rand_by_domain[$i]}  )
-            fi
+#             if [[ "${rand_by_domain[$i]}" -ge "8" && "${rand_by_domain[$i]}" -le "11" ]]; then
+#                 richable_eu+=( ${rand_by_domain[$i]}  )
+#             fi
 
-            if [[ "${rand_by_domain[$i]}" -gt "4" &&  "${rand_by_domain[$i]}" -le "7" ]]; then
-                richable_us+=( ${rand_by_domain[$i]}  )
-            fi
+#             if [[ "${rand_by_domain[$i]}" -gt "4" &&  "${rand_by_domain[$i]}" -le "7" ]]; then
+#                 richable_us+=( ${rand_by_domain[$i]}  )
+#             fi
 
-            if [[ "${rand_by_domain[$i]}" -ge "12" ]]; then
-                richable_as+=( ${rand_by_domain[$i]}  )
-            fi
+#             if [[ "${rand_by_domain[$i]}" -ge "12" ]]; then
+#                 richable_as+=( ${rand_by_domain[$i]}  )
+#             fi
 
-            richable+=( ${rand_by_domain[$i]} )
-        fi
+#             richable+=( ${rand_by_domain[$i]} )
+#         fi
 
-        i=$(($i+1))
-    done
+#         i=$(($i+1))
+#     done
 
-    server_found="1"
-    if [[ "$continent" == "EU" ]]; then
-        len_eu=${#richable_eu[@]}
-        if [[ "$len_eu" -gt "0" ]]; then
-            richable=( ${richable_eu[*]} )
-            echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
-        fi
-        if [[ "$len_eu" == "0" ]]; then
-            continent="EU"
-            echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
-            len_us=${#richable_us[@]}
-            if [[ "$len_us" -gt "0" ]]; then
-                richable=( ${richable_us[*]} )
-                echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
-            fi
-            if [[ "$len_us" == "0" ]]; then
-                continent="US"
-                echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
-                server_found="0"
-            fi
-        fi
-    elif [[ "$continent" == "US" ]]; then
-        len_us=${#richable_us[@]}
-        if [[ "$len_us" -gt "0" ]]; then
-            richable=( ${richable_us[*]} )
-            echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
-        fi
-        if [[ "$len_us" == "0" ]]; then
-            continent="US"
-            echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
-            len_as=${#richable_as[@]}
-            if [[ "$len_as" -gt "0" ]]; then
-                richable=( ${richable_as[*]} )
-                echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
-            fi
-            if [[ "$len_as" == "0" ]]; then
-                continent="AS"
-                echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
-                len_eu=${#richable_eu[@]}
-                if [[ "$len_eu" -gt "0" ]]; then
-                    richable=( ${richable_eu[*]} )
-                    echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
-                fi
-                if [[ "$len_eu" == "0" ]]; then
-                    continent="EU"
-                    echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
-                    server_found="0"
-                fi
-            fi
-        fi
-    elif [[ "$continent" == "AS" ]]; then
-        len_as=${#richable_as[@]}
-        if [[ "$len_as" -gt "0" ]]; then
-            richable=( ${richable_as[*]} )
-            echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
-        fi
-        if [[ "$len_as" == "0" ]]; then
-            continent="AS"
-            echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
-            len_us=${#richable_us[@]}
-            if [[ "$len_us" -gt "0" ]]; then
-                richable=( ${richable_us[*]} )
-                echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
-            fi
-            if [[ "$len_us" == "0" ]]; then
-                continent="US"
-                echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
-                len_eu=${#richable_eu[@]}
-            if [[ "$len_eu" -gt "0" ]]; then
-                richable=( ${richable_eu[*]} )
-                echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
-            fi
-            if [[ "$len_eu" == "0" ]]; then
-                continent="EU"
-                echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
-                server_found="0"
-            fi
-        fi
-    fi
-    else
-        len=${#richable[@]}
-        if [[ "$len" -gt "0" ]]; then
-            richable=( ${richable[*]} )
-            echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
-        fi
+#     server_found="1"
+#     if [[ "$continent" == "EU" ]]; then
+#         len_eu=${#richable_eu[@]}
+#         if [[ "$len_eu" -gt "0" ]]; then
+#             richable=( ${richable_eu[*]} )
+#             echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
+#         fi
+#         if [[ "$len_eu" == "0" ]]; then
+#             continent="EU"
+#             echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
+#             len_us=${#richable_us[@]}
+#             if [[ "$len_us" -gt "0" ]]; then
+#                 richable=( ${richable_us[*]} )
+#                 echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
+#             fi
+#             if [[ "$len_us" == "0" ]]; then
+#                 continent="US"
+#                 echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
+#                 server_found="0"
+#             fi
+#         fi
+#     elif [[ "$continent" == "US" ]]; then
+#         len_us=${#richable_us[@]}
+#         if [[ "$len_us" -gt "0" ]]; then
+#             richable=( ${richable_us[*]} )
+#             echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
+#         fi
+#         if [[ "$len_us" == "0" ]]; then
+#             continent="US"
+#             echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
+#             len_as=${#richable_as[@]}
+#             if [[ "$len_as" -gt "0" ]]; then
+#                 richable=( ${richable_as[*]} )
+#                 echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
+#             fi
+#             if [[ "$len_as" == "0" ]]; then
+#                 continent="AS"
+#                 echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
+#                 len_eu=${#richable_eu[@]}
+#                 if [[ "$len_eu" -gt "0" ]]; then
+#                     richable=( ${richable_eu[*]} )
+#                     echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
+#                 fi
+#                 if [[ "$len_eu" == "0" ]]; then
+#                     continent="EU"
+#                     echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
+#                     server_found="0"
+#                 fi
+#             fi
+#         fi
+#     elif [[ "$continent" == "AS" ]]; then
+#         len_as=${#richable_as[@]}
+#         if [[ "$len_as" -gt "0" ]]; then
+#             richable=( ${richable_as[*]} )
+#             echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
+#         fi
+#         if [[ "$len_as" == "0" ]]; then
+#             continent="AS"
+#             echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
+#             len_us=${#richable_us[@]}
+#             if [[ "$len_us" -gt "0" ]]; then
+#                 richable=( ${richable_us[*]} )
+#                 echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
+#             fi
+#             if [[ "$len_us" == "0" ]]; then
+#                 continent="US"
+#                 echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
+#                 len_eu=${#richable_eu[@]}
+#             if [[ "$len_eu" -gt "0" ]]; then
+#                 richable=( ${richable_eu[*]} )
+#                 echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
+#             fi
+#             if [[ "$len_eu" == "0" ]]; then
+#                 continent="EU"
+#                 echo -e "${WORNING} ${CYAN}All Bootstrap in $continent are offline, checking other location...${NC}" && sleep 1
+#                 server_found="0"
+#             fi
+#         fi
+#     fi
+#     else
+#         len=${#richable[@]}
+#         if [[ "$len" -gt "0" ]]; then
+#             richable=( ${richable[*]} )
+#             echo -e "${ARROW} ${CYAN}Reachable servers: ${richable[*]}${NC}"
+#         fi
     
-        if [[ "$len" == "0" ]]; then
-            Server_offline=1
-            return 1
-        fi
-    fi
+#         if [[ "$len" == "0" ]]; then
+#             Server_offline=1
+#             return 1
+#         fi
+#     fi
 
-    if [[ "$server_found" == "0" ]]; then
-        len=${#richable[@]}
-        if [[ "$len" == "0" ]]; then
-            Server_offline=1
-            return 1
-        fi
-    fi
+#     if [[ "$server_found" == "0" ]]; then
+#         len=${#richable[@]}
+#         if [[ "$len" == "0" ]]; then
+#             Server_offline=1
+#             return 1
+#         fi
+#     fi
 
-    Server_offline=0
-}
+#     Server_offline=0
+# }
 
-function bootstrap_geolocation(){
-    IP=$WANIP
-    ip_output=$(curl -s -m 10 http://ip-api.com/json/$1?fields=status,country,timezone 2>/dev/null | jq . 2>/dev/null)
-    ip_status=$( jq -r .status 2>/dev/null <<< "$ip_output")
+# function bootstrap_geolocation(){
+#     IP=$WANIP
+#     ip_output=$(curl -s -m 10 http://ip-api.com/json/$1?fields=status,country,timezone 2>/dev/null | jq . 2>/dev/null)
+#     ip_status=$( jq -r .status 2>/dev/null <<< "$ip_output")
 
-    if [[ "$ip_status" == "success" ]]; then
-        country=$(jq -r .country <<< "$ip_output")
-        org=$(jq -r .org <<< "$ip_output")
-        continent=$(jq -r .timezone <<< "$ip_output")
-    else
-        country="UKNOW"
-        continent="UKNOW"
-    fi
+#     if [[ "$ip_status" == "success" ]]; then
+#         country=$(jq -r .country <<< "$ip_output")
+#         org=$(jq -r .org <<< "$ip_output")
+#         continent=$(jq -r .timezone <<< "$ip_output")
+#     else
+#         country="UKNOW"
+#         continent="UKNOW"
+#     fi
 
-    continent=$(cut -f1 -d"/" <<< "$continent" )
+#     continent=$(cut -f1 -d"/" <<< "$continent" )
 
-    if [[ "$continent" =~ "Europe" ]]; then
-        continent="EU"
-    elif [[ "$continent" =~ "America" ]]; then
-        continent="US"
-    elif [[ "$continent" =~ "Asia" ]]; then
-        continent="AS"
-    else
-        continent="ALL"
-    fi
+#     if [[ "$continent" =~ "Europe" ]]; then
+#         continent="EU"
+#     elif [[ "$continent" =~ "America" ]]; then
+#         continent="US"
+#     elif [[ "$continent" =~ "Asia" ]]; then
+#         continent="AS"
+#     else
+#         continent="ALL"
+#     fi
 
-    echo -e "${ARROW} ${CYAN}Selecting bootstrap server....${NC}"
-    echo -e "${ARROW} ${CYAN}Node Location -> IP:$IP, Country: $country, Continent: $continent ${NC}"
-    echo -e "${ARROW} ${CYAN}Searching in $continent....${NC}"
-    bootstrap_server $continent
-}
+#     echo -e "${ARROW} ${CYAN}Selecting bootstrap server....${NC}"
+#     echo -e "${ARROW} ${CYAN}Node Location -> IP:$IP, Country: $country, Continent: $continent ${NC}"
+#     echo -e "${ARROW} ${CYAN}Searching in $continent....${NC}"
+#     bootstrap_server $continent
+# }
 
 function get_ip(){
     WANIP=$(curl -SsL -m 5 https://api4.my-ip.io/ip 2>/dev/null | tr -dc '[:alnum:].')
